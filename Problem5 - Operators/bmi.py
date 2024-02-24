518cd7f693404900b6629ca4cdb28743806c64a7
Jeet Mehta <mehta.jeet97@gmail.com>
2024-02-24 00:44:20 -0500

    second Problem 5 added

---
+++ Problem5 - Operators/bmi.py
@@ -0,0 +1,10 @@
+# Value for weight in kgs
+weight = 75
+# Value for height in meters
+height = 1.77
+
+# Calculate using formula for BMI
+bmi = weight/(height ** height)
+
+# Print Output
+print(bmi)
--- Problem5 - Operators/temperature.py
+++ Problem5 - Operators/temperature.py
@@ -1,6 +1,6 @@
 # Current temperature in Fahrenheit
 temp_f = 42
-temp_c = (temp_f - 32)/1.8  #Formula
+temp_c = (temp_f - 32)/1.8  # Formula
 
-#Printing temp in Celsisus
+# Printing temp in Celsisus
 print(temp_c)
