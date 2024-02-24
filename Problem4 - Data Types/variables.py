e389b1bd680c3b0317e3d2ae8fecff4c5d66a842
Jeet Mehta <mehta.jeet97@gmail.com>
2024-02-24 00:30:10 -0500

    Problem4 completed

---
+++ Problem4 - Data Types/variables.py
@@ -0,0 +1,7 @@
+name = 'Erlich Bachman' #String
+user_id = 16180339887   #Integer
+progress = 0.75         #Float
+exp = 60                #Integer
+verified = True         #Boolean
+
+print(name + ' ' + str(user_id) + ' ' + str(progress) + ' ' + str(exp) + ' ' + str(verified))
