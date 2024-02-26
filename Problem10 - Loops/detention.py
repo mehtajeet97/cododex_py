6b593824f54a019b48e68173cb5e3733afc32afd
Jeet Mehta <mehta.jeet97@gmail.com>
2024-02-26 01:31:50 -0500

    Problem10 detention.py added

---
+++ Problem10 - Loops/detention.py
@@ -0,0 +1,6 @@
+# Input number of times to write
+num = int(input("How many times to write it on the whiteboard?\n"))
+
+# Simple for loop
+for i in range(num):
+    print("I will not use Snapchat in class")
