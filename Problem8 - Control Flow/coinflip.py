c0237437747f3fae090ddd26ae200ee1a7aa6d21
Jeet Mehta <mehta.jeet97@gmail.com>
2024-02-25 23:28:32 -0500

    Problem8 commented

--- Problem8 - Control Flow/coinflip.py
+++ Problem8 - Control Flow/coinflip.py
@@ -2,6 +2,7 @@ import random
 
 num = random.randint(0, 1)   # Generates a random number that's either 0 or 1
 
+# Randomly generates Output
 if num > 0.5:
   print('Heads')
 else:
---
+++ Problem8 - Control Flow/grades.py
@@ -0,0 +1,8 @@
+# Input Grade from User
+grade = int(input("Enter your grade: "))
+
+# Output based on If-condition
+if grade>=55:
+    print("Cngratulations. You PASSED!!!")
+else:
+    print("So Sorry. You FAILED!!!")
